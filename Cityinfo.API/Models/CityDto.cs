fb8b63a15f39264acf03b11f6d6929f5ad942cfc
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2024-08-05 22:43:17 -0700

    new update for returning child resources

--- Cityinfo.API/Models/CityDto.cs
+++ Cityinfo.API/Models/CityDto.cs
@@ -1,9 +1,21 @@
-﻿namespace Cityinfo.API.Models
+﻿namespace CityInfo.API.Models
 {
     public class CityDto
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; }
+        public string? Description { get; set; }
+
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        //using ICollection to avoid null reference issues
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set;} 
+            = new List<PointOfInterestDto>();
     }
 }
